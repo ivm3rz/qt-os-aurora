5077b355c855b8896fba52b086aad29adbe47ee8
Igor Merzlyakov <igor.merzlyakov@gmail.com>
2024-01-29 23:25:11 +0300

    volers: Доработка отображения таблицы извержений вулканов

--- 03-volers/main.cpp
+++ 03-volers/main.cpp
@@ -8,11 +8,22 @@
 #include <QtWidgets/QTableWidget>
 
 
+enum CsvColumn
+{
+     Year,
+     Month,
+     Day,
+     FlagTsunami,
+     FlagEarthquake,
+     VolcanoName,
+};
+
+
 auto date( const QStringList& row )
 {
-     const auto year = row.at( 0 ).toInt();
-     const auto month = row.at( 1 ).toInt();
-     const auto day = row.at( 2 ).toInt();
+     const auto year = row.at( Year ).toInt();
+     const auto month = row.at( Month ).toInt();
+     const auto day = row.at( Day ).toInt();
 
      return month and day
           ? QDate{ year, month, day }.toString( Qt::ISODate )
@@ -28,11 +39,12 @@ int main( int argc, char *argv[] )
      QFile file{ ":/significant-volcanic-eruption-database.csv" };
 
      QTableWidget widget;
-     widget.setColumnCount( 1 );
+     QStringList headerLabels{ "Дата/год", "Цунами", "Землетрясение", "Название вулкана" };
+     widget.setColumnCount( headerLabels.size() );
      widget.setShowGrid( true );
      widget.setSelectionMode( QAbstractItemView::SingleSelection );
      widget.setSelectionBehavior( QAbstractItemView::SelectRows );
-     widget.setHorizontalHeaderLabels( QStringList() << "Дата/год" << "Флаг цунами" );
+     widget.setHorizontalHeaderLabels( headerLabels );
      widget.horizontalHeader()->setStretchLastSection( true );
 
      if( file.open( QFile::ReadOnly | QFile::Text ) )
@@ -50,9 +62,21 @@ int main( int argc, char *argv[] )
                     dateItem->setData( Qt::DisplayRole, date( values ) );
                     widget.setItem( index, 0, dateItem );
                }
+               if( const auto tsunamiItem = new QTableWidgetItem() )
+               {
+                    tsunamiItem->data( Qt::CheckStateRole );
+                    tsunamiItem->setCheckState( values.at( FlagTsunami ).contains( "Tsunami" ) ? Qt::Checked : Qt::Unchecked );
+                    widget.setItem( index, 1, tsunamiItem );
+               }
+               if( const auto earthquakeItem = new QTableWidgetItem() )
+               {
+                    earthquakeItem->data( Qt::CheckStateRole );
+                    earthquakeItem->setCheckState( values.at( FlagEarthquake ).contains( "Earthquake" ) ? Qt::Checked : Qt::Unchecked );
+                    widget.setItem( index, 2, earthquakeItem );
+               }
+               widget.setItem( index, 3, new QTableWidgetItem( values.at( VolcanoName ) ) );
           }
      }
-
      widget.resizeColumnsToContents();
      widget.setWindowTitle( QObject::tr( "Данные о наиболее значимых вулканических извержениях" ) );
      widget.show();
