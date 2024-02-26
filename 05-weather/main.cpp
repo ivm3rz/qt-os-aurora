8f829c5f786f5c29cabaab1f52f01a28b039b6db
Igor Merzlyakov <igor.merzlyakov@gmail.com>
2024-02-27 00:05:10 +0300

    weather: Небольшие доработки отображения данных местоположения

--- 05-weather/main.cpp
+++ 05-weather/main.cpp
@@ -14,11 +14,13 @@ int main( int argc, char *argv[] )
 
      QWidget widget;
 
-     const auto formLayout = new QFormLayout( &widget );
+     const auto formLayout = new QFormLayout;
      const auto latitude = new QLabel( &widget );
      const auto longitude = new QLabel( &widget );
+     const auto location = new QLabel( &widget );
      formLayout->addRow( QObject::tr( "Latitude:" ), latitude );
-     formLayout->addRow( QObject::tr( "Longitude" ), longitude );
+     formLayout->addRow( QObject::tr( "Longitude:" ), longitude );
+     formLayout->addRow( QObject::tr( "Location:" ), location );
      widget.setLayout( formLayout );
 
      QNetworkAccessManager networkManager;
@@ -27,22 +29,26 @@ int main( int argc, char *argv[] )
      QObject::connect(
           reply
           , &QNetworkReply::readyRead
-          , [ reply, latitude, longitude ]
+          , [ reply, latitude, longitude, location ]
           {
                const auto& json = reply->readAll();
                qDebug().noquote() << json;
                const auto& document = QJsonDocument::fromJson( json );
                latitude->setNum( document[ "latitude" ].toDouble() );
                longitude->setNum( document[ "longitude" ].toDouble() );
+               location->setText( document[ "country" ].toString() + "/" + document[ "city" ].toString() );
           }
      );
 
      QObject::connect(
           reply
           , &QNetworkReply::errorOccurred
-          , [ reply ]
+          , [ reply, latitude, longitude, location ]
           {
                qDebug() << "Error: " + reply->errorString();
+               latitude->setText( "--" );
+               longitude->setText( "--" );
+               location->setText( "-- " );
           }
      );
 
@@ -52,6 +58,7 @@ int main( int argc, char *argv[] )
           , reply
           , &QNetworkReply::deleteLater
      );
+     widget.resize( 320, 240 );
      widget.setWindowTitle( "Weather⛅" );
      widget.show();
      return app.exec();
