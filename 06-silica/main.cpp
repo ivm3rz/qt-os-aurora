59f98be70c84b2464f58b796f8983319450f1ca3
Igor Merzlyakov <igor.merzlyakov@gmail.com>
2024-03-14 22:50:55 +0300

    silica: Исправление сборки с libsailfishapp

--- 06-silica/main.cpp
+++ 06-silica/main.cpp
@@ -5,11 +5,15 @@
 #ifdef USE_SAILFISH_APP
 #include <sailfishapp/sailfishapp.h>
 #else
-#include <libauororaapp/libauroraapp.h>
+#include <auroraapp/auroraapp.h>
 #endif
 
 int main( int argc, char *argv[] )
 {
+#ifdef USE_SAILFISH_APP
      // FIXME: unable to use broken `Aurora::Application::main` (as well as `Auorora::Application::pathToMainQml`)
      return SailfishApp::main( argc, argv );
+#else
+     return Aurora::Application::main( argc, argv );
+#endif
 }
