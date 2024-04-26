17a5175354cdc7b0a3e4e6425c66d6d7f64aecfa
Igor Merzlyakov <igor.merzlyakov@gmail.com>
2024-04-26 15:11:54 +0300

    locale: Добавлена поддержка локализации приложения на русском языке

---
+++ 10-locale/LocaleApp_ru.ts
@@ -0,0 +1,83 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru">
+<context>
+    <name>AccountPage</name>
+    <message>
+        <location filename="qml/pages/AccountPage.qml" line="14"/>
+        <source>User info</source>
+        <translation>Сведения о пользователе</translation>
+    </message>
+    <message>
+        <location filename="qml/pages/AccountPage.qml" line="21"/>
+        <source>Name</source>
+        <translation>Имя</translation>
+    </message>
+    <message>
+        <location filename="qml/pages/AccountPage.qml" line="32"/>
+        <source>Surname</source>
+        <translation>Фамилия</translation>
+    </message>
+    <message>
+        <location filename="qml/pages/AccountPage.qml" line="41"/>
+        <source>Email</source>
+        <translation>Электронная почта</translation>
+    </message>
+    <message>
+        <location filename="qml/pages/AccountPage.qml" line="42"/>
+        <source>Enter your email</source>
+        <translation>Введите адрес электронной почты</translation>
+    </message>
+    <message>
+        <location filename="qml/pages/AccountPage.qml" line="55"/>
+        <source>Favorite progamming language</source>
+        <translation>Предпочитаемый язык программирования</translation>
+    </message>
+    <message>
+        <location filename="qml/pages/AccountPage.qml" line="68"/>
+        <source>I consent to the processing of personal data</source>
+        <translation>Я даю согласие на обработку персональных данных</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="qml/cover/CoverPage.qml" line="8"/>
+        <source>LocaleApp</source>
+        <translation>Локализованное приложение</translation>
+    </message>
+</context>
+<context>
+    <name>RegisterPage</name>
+    <message>
+        <location filename="qml/pages/RegisterPage.qml" line="15"/>
+        <source>User registration</source>
+        <translation>Регистрация пользователя</translation>
+    </message>
+    <message>
+        <location filename="qml/pages/RegisterPage.qml" line="22"/>
+        <source>Phone number</source>
+        <translation>Номер телефона</translation>
+    </message>
+    <message>
+        <location filename="qml/pages/RegisterPage.qml" line="23"/>
+        <source>Enter mobile phone number</source>
+        <translation>Введите номер телефона</translation>
+    </message>
+    <message>
+        <location filename="qml/pages/RegisterPage.qml" line="36"/>
+        <source>Login</source>
+        <translation>Логин</translation>
+    </message>
+    <message>
+        <location filename="qml/pages/RegisterPage.qml" line="37"/>
+        <source>Enter your login</source>
+        <translation>Введите логин</translation>
+    </message>
+    <message>
+        <location filename="qml/pages/RegisterPage.qml" line="51"/>
+        <source>Next</source>
+        <translation>Далее</translation>
+    </message>
+</context>
+</TS>
