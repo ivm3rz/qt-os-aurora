8f0a5164eb84ef03ad67ae283bab168f8977763b
Igor Merzlyakov <igor.merzlyakov@gmail.com>
2024-04-26 16:24:57 +0300

    locale: Исправление локализации приложения

---
+++ 10-locale/LocaleApp.ts
@@ -0,0 +1,83 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
+<context>
+    <name>AccountPage</name>
+    <message>
+        <location filename="qml/pages/AccountPage.qml" line="14"/>
+        <source>User info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/pages/AccountPage.qml" line="21"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/pages/AccountPage.qml" line="32"/>
+        <source>Surname</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/pages/AccountPage.qml" line="41"/>
+        <source>Email</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/pages/AccountPage.qml" line="42"/>
+        <source>Enter your email</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/pages/AccountPage.qml" line="55"/>
+        <source>Favorite progamming language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/pages/AccountPage.qml" line="68"/>
+        <source>I consent to the processing of personal data</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="qml/cover/CoverPage.qml" line="8"/>
+        <source>LocaleApp</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RegisterPage</name>
+    <message>
+        <location filename="qml/pages/RegisterPage.qml" line="15"/>
+        <source>User registration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/pages/RegisterPage.qml" line="22"/>
+        <source>Phone number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/pages/RegisterPage.qml" line="23"/>
+        <source>Enter mobile phone number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/pages/RegisterPage.qml" line="36"/>
+        <source>Login</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/pages/RegisterPage.qml" line="37"/>
+        <source>Enter your login</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/pages/RegisterPage.qml" line="51"/>
+        <source>Next</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
